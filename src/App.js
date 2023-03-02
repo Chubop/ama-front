f1ad4ea9e20cfc42bd7a0861dd8ee9b0fff41d7b
chubop <samuelokirby@gmail.com>
2023-03-02 12:51:58 -0500

    changed default font

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import SignIn from './auth/SignIn';
 import Home from './components/Home';
+import NavBar from './components/NavBar';
 import './style.css'; 
 
 
 const theme = createTheme({
   typography: {
-    fontFamily: [
-      'Courier New'
-    ],
+    // fontFamily: [
+    //   ''
+    // ],
   },
 });
 
@@ -20,6 +21,7 @@ function App() {
       <CssBaseline />
       <div className="App">
         <header className="App-header">
+          <NavBar/>
           <Routes>
             <Route exact path="/" element={<Home/>}/>
             <Route exact path="/signin" element={<SignIn/>}/>
