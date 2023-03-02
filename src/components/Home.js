d172db9154c134a5b4e1923fa6a285d2883ff92e
chubop <samuelokirby@gmail.com>
2023-03-02 12:52:33 -0500

    changed home page to support user data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,80 @@
 import { Typography, Grid, Button } from "@mui/material"
 import supabase from "../auth/supabase";
 import GetUserData from "../auth/GetUserData";
+import { useEffect, useState } from "react";
 
 export default function Home(props){
+  
+    const [user, setUser] = useState({})
+    const [loading, setLoading] = useState(true);
+    const [userName, setUserName] = useState(null)
+
+    GetUserData().then(
+      function(value){
+        setUser(value);
+      },
+      function(error){
+        console.log(error)
+      }
+    );
+
+    useEffect(() => {
+      if(user.id){
+          setUserName(getUsernameFromUUID(user.id));
+          setLoading(false);
+      }
+    }, [user])
+
     
-    const user = GetUserData();
+    async function getUsernameFromUUID(uuid) {
+      const { data, error } = await supabase
+        .from('profiles')
+        .select('username')
+        .eq('id', uuid)
+        .single()
+    
+      console.log(data);
+      setUserName(data)
+      if (error) {
+        console.error(error)
+        return null
+      }
+      return data
+    }
+
 
     async function handleSignOut(){
       await supabase.auth.signOut();
+      window.location.reload();
     }
+
+    function isLoggedIn(){
+      return user ? true : false
+    }
+
 
     return(
       <Grid container direction="column">
           <Grid item>
-            Welcome
+            {/* Welcome, {userName.username} */}
           </Grid>
           <Grid item>
             <Typography>
-              {user ? "Logged In" : "Logged Out"}
+              {/* {isLoggedIn() ? "Logged In" : "Logged Out"} */}
             </Typography>
           </Grid>
           <Grid item>
             <Button onClick={() => handleSignOut()}>Log Out</Button>
           </Grid>
+          <Grid item>
+            <Button onClick={() => console.log(user)}>log user object</Button>
+          </Grid>
+          <Grid item>
+            <Button onClick={() => {
+              console.log(userName)
+            }}>log username</Button>
+          </Grid>
       </Grid>
-
     )
+
 }
