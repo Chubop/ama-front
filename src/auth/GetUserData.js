a642958665c77c5a4a7b3295014e1677df4caf72
chubop <samuelokirby@gmail.com>
2023-03-02 12:51:37 -0500

    added getUser console logging

--- src/auth/GetUserData.js
+++ src/auth/GetUserData.js
@@ -3,21 +3,15 @@ import supabase from "./supabase";
 
 export default async function GetUserData(){ 
     const [user, setUser] = useState({})
-    let x = {'h': 1}
-    let y = {...x}
+
     useEffect(() => {
         async function getUser(){
-            await supabase.auth.getUser().then((value) => {
-                if(value.data?.user) {
-                    console.log(y)
-                    console.log(user);
-                    console.log(value.data.user);
-                    setUser({...x});
-                    console.log(user);
-                }
-            })
+            const request = await supabase.auth.getUser();
+            setUser(request.data.user);
+            return request;
         }
         getUser();
     }, []);
+    
     return user;
 }
